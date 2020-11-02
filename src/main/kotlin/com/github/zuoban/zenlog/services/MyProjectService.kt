6458325d42b20dd37274880137d939937a7d4717
GitHub Action <action@github.com>
2020-11-02 03:46:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/zuoban/zenlog/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zuoban/zenlog/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zuoban.zenlog.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zuoban.zenlog.MyBundle
 
 class MyProjectService(project: Project) {
 
